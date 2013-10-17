4ce242428e368bacdc28c4ff0fd03c8419a0b942
acarre <adrienbenj@gmail.com>
2013-10-17 12:02:34 +0200

    display copyright

--- src/application.bat
+++ src/application.bat
@@ -1,3 +1,4 @@
-### application.bat - 201x - Carré Adrien ###
+### application.bat - 2013 - Carré Adrien ###
+echo "Copyright company - 2013"
 echo "Build : 10:00"
-echo "version 1"
+echo "version 1.1"
