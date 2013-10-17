d39706e8e2a2588b5dabde4ba9bbd18aa24a364e
acarre <adrienbenj@gmail.com>
2013-10-17 14:38:35 +0200

    update minor version2

--- test/test_app.bat
+++ test/test_app.bat
@@ -1,5 +1,5 @@
-### test_app.bat - 2013 - Carré Adrien 1.2.0-b ###
-version = "1.2.0-b"
+### test_app.bat - 2013 - Carré Adrien 1.2.1 ###
+version = "1.2.1"
 build_hour = "11H00"
 
 echo "Build ${build_hour}"
